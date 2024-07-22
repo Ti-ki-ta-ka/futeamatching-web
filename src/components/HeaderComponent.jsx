314e9fa44ea916b4684aa775274126f6db878494
임성우 <seongwoorang@naver.com>
2024-07-22 20:50:36 +0900

    drawer 추가

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,13 +1,18 @@
 // HeaderComponent.jsx
-import React from 'react';
-import { Group, Text, Button, ActionIcon, TextInput, Box } from '@mantine/core';
-import { IconSearch, IconMenu2 } from '@tabler/icons-react';
+import React, { useState } from 'react';
+import { Group, Text, Button, ActionIcon, TextInput, Box, Drawer, Divider, Stack } from '@mantine/core';
+import { IconSearch, IconMenu2, IconHome, IconUsers } from '@tabler/icons-react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../AuthContext'; 
 
 const HeaderComponent = () => {
+  const [drawerOpened, setDrawerOpened] = useState(false);
   const navigate = useNavigate();
   const { isAuthenticated, logout } = useAuth();
+
+  const toggleDrawer = () => {
+    setDrawerOpened((prev) => !prev);
+  };
 
   return (
     <Box
@@ -20,7 +25,7 @@ const HeaderComponent = () => {
     >
       <Group justify="space-between" mt="md">
         <Group spacing="xs">
-          <ActionIcon variant="default" size="lg">
+          <ActionIcon variant="default" size="lg" onClick={toggleDrawer}>
             <IconMenu2 />
           </ActionIcon>
           <Text weight={700} size="xl">
@@ -75,6 +80,58 @@ const HeaderComponent = () => {
         <Button variant="outline" color="green" onClick={() => navigate('/')}>매칭하기</Button>
         <Button variant="outline" color="green" onClick={() => navigate('/team')}>팀 구하기</Button>
       </Group>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={toggleDrawer}
+        title="메뉴"
+        padding="xl"
+        size="md"
+      >
+        <Stack spacing="sm">
+          <Button 
+            fullWidth 
+            variant="subtle" 
+            color="green"
+            leftIcon={<IconHome size={20} />} 
+            onClick={() => navigate('/')}
+          >
+            매칭하기
+          </Button>
+          <Button 
+            fullWidth 
+            variant="subtle" 
+            color="green"
+            leftIcon={<IconUsers size={20} />} 
+            onClick={() => navigate('/login')}
+          >
+            프로필 변경하기
+          </Button>
+          <Button 
+            fullWidth 
+            variant="subtle" 
+            color="green"
+            leftIcon={<IconUsers size={20} />} 
+            onClick={() => navigate('/signup')}
+          >
+            비밀번호 수정하기
+          </Button>
+        </Stack>
+        <Divider my="lg" />
+        {isAuthenticated && (
+          <Box mt="auto">
+            <Button 
+              fullWidth 
+              variant="outline" 
+              color="black" 
+              onClick={logout}
+              mt="lg"
+            >
+              로그아웃
+            </Button>
+          </Box>
+        )}
+      </Drawer>
     </Box>
   );
 };
