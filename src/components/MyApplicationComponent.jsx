d335f340d917368dba99a65f9ed5776e78bd75c9
임성우 <seongwoorang@naver.com>
2024-07-25 17:13:34 +0900

    매치 신청 내역 화면 grid 추가

--- src/components/MyApplicationComponent.jsx
+++ src/components/MyApplicationComponent.jsx
@@ -1,46 +1,50 @@
 import React from 'react';
-import { Card, Group, Text, Badge } from '@mantine/core';
+import { Card, Group, Text, Badge, SimpleGrid } from '@mantine/core';
 
 const MyApplicationComponent = ({ applications }) => {
   return (
     <>
       <form
         style={{
-          width: 480,
-          margin: "auto",
+          width: '90%',
+          margin: 'auto',
           marginTop: 100
         }}
       >
         <Text fw={500} ta="center" mb="md">매치 신청 내역 ⚽</Text>
-        {applications.map((application) => (
-          <Card key={application.id} shadow="sm" padding="lg" radius="md" withBorder mb="md">
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-              <Group>
-                <Text fw={700} size="lg">
-                  {application.matchPost.title}
-                </Text>
-                <Badge color="green" variant="light">
-                  {application.approveStatus}
-                </Badge>
-                <Badge color="blue" variant="light">
-                  {application.matchPost.region}
-                </Badge>
-              </Group>
-            </div>
+        <SimpleGrid cols={3} spacing="lg" breakpoints={[
+          { maxWidth: 'sm', cols: 1 }
+        ]}>
+          {applications.map((application) => (
+            <Card key={application.id} shadow="sm" padding="lg" radius="md" withBorder mb="md">
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Group>
+                  <Text fw={700} size="lg">
+                    {application.matchPost.title}
+                  </Text>
+                  <Badge color="green" variant="light">
+                    {application.approveStatus}
+                  </Badge>
+                  <Badge color="blue" variant="light">
+                    {application.matchPost.region}
+                  </Badge>
+                </Group>
+              </div>
 
-            <div style={{ marginTop: '10px' }}>
-              <Text size="md" style={{ marginTop: '10px' }}>
-                <Text fw={700} component="span">경기 장소:</Text> {application.location}
-              </Text>
-              <Text size="md">
-                <Text fw={700} component="span">내용:</Text> {application.matchPost.content}
-              </Text>
-              <Text size="md">
-                <Text fw={700} component="span">날짜:</Text> {new Date(application.matchPost.matchDate).toLocaleString()}
-              </Text>
-            </div>
-          </Card>
-        ))}
+              <div style={{ marginTop: '10px' }}>
+                <Text size="md" style={{ marginTop: '10px' }}>
+                  <Text fw={700} component="span">경기 장소:</Text> {application.location}
+                </Text>
+                <Text size="md">
+                  <Text fw={700} component="span">내용:</Text> {application.matchPost.content}
+                </Text>
+                <Text size="md">
+                  <Text fw={700} component="span">날짜:</Text> {new Date(application.matchPost.matchDate).toLocaleString()}
+                </Text>
+              </div>
+            </Card>
+          ))}
+        </SimpleGrid>
       </form>
     </>
   );
