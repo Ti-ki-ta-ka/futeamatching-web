c0c185d976583bc212c41ebc654c8283066a2c2a
임성우 <seongwoorang@naver.com>
2024-07-31 15:39:59 +0900

    baseUrl 변경

--- src/api/client.js
+++ src/api/client.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: 'http://ec2-3-38-213-7.ap-northeast-2.compute.amazonaws.com:9090/api/v1',
+  baseURL: 'http://www.futeamatching.com/api/v1',
   headers: {
     "Access-Control-Allow-Origin": "*", // CORS
   },
 });
 
 export const client2 = axios.create({
-  baseURL: 'http://ec2-3-38-213-7.ap-northeast-2.compute.amazonaws.com:9090/api/v2',
+  baseURL: 'http://www.futeamatching.com/api/v2',
   headers: {
     "Access-Control-Allow-Origin": "*", // CORS
   }
