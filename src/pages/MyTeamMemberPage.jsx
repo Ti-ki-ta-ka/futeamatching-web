a181b17bece57b8f89c90ce61098bb11909171b9
kozy5 <tage9592@naver.com>
2024-08-09 21:03:44 +0900

    내 팀 멤버 보기 기능 추가

---
+++ src/pages/MyTeamMemberPage.jsx
@@ -0,0 +1,11 @@
+import MyTeamMemberContainer from "../components/MyTeamMemberContainer"
+
+const MyTeamMemberPage = () => {
+  return (
+    <div>
+      <MyTeamMemberContainer />
+    </div>
+  );
+};
+
+export default MyTeamMemberPage;
